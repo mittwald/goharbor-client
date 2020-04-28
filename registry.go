9b74ae7e80fc028cd507315da28c825f6b69a354
Eike David Lenz <e.lenz@mittwald.de>
2020-04-28 15:12:48 +0200

    add create and list registry methods

--- registry.go
+++ registry.go
@@ -11,6 +11,25 @@ type RegistryClient struct {
 	*Client
 }
 
+// CreateRegistry
+// Create a registry
+func (s *RegistryClient) CreateRegistry(r Registry) error {
+	resp, _, errs := s.NewRequest(gorequest.POST, "").
+		Send(r).
+		End()
+	return CheckResponse(errs, resp, 201)
+}
+
+// ListRegistries
+// Get a list of registries
+func (s *RegistryClient) ListRegistries(name string) ([]Registry, error) {
+	var r []Registry
+	resp, _, errs := s.NewRequest(gorequest.GET, "").
+		Query(name).
+		EndStruct(&r)
+	return r, CheckResponse(errs, resp, 200)
+}
+
 // GetRegistryByID
 // Get a registry by ID
 func (s *RegistryClient) GetRegistryByID(id int64) (Registry, error) {
