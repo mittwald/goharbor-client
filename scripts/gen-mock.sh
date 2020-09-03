218c3fed92509abe6e553ed2bd70efc9f2aff275
Eike David Lenz <e.lenz@mittwald.de>
2020-09-03 16:12:10 +0200

    add autogenerated code for v2

--- scripts/gen-mock.sh
+++ scripts/gen-mock.sh
@@ -7,15 +7,15 @@ if [[ "${1}" = *"v1"* ]]; then
   echo "generating mocks using the v1 API"
   docker run --rm -e GOPATH="${HOME}/go:/go" -v "${HOME}:${HOME}" -w "$(pwd)" ${MOCKERY_IMAGE} \
   --name ClientService --dir apiv1/internal/api/client/products/ \
-  --output ./apiv1/mocks --filename client_service.go --structname MockClientService --log-level debug
+  --output ./apiv1/mocks --filename client_service.go --structname MockClientService
   printf "%s\n" 1 i "${BUILD_FLAG}" . w | ed -s "${MOCK_FILE}" &>/dev/null
 fi
 
 if [[ "${1}" = *"v2"* ]]; then
   MOCK_FILE=apiv2/mocks/client_service.go
-  echo "generating mocks using the v1 API"
+  echo "generating mocks using the v2 API"
   docker run --rm -e GOPATH="${HOME}/go:/go" -v "${HOME}:${HOME}" -w "$(pwd)" ${MOCKERY_IMAGE} \
   --name ClientService --dir apiv2/internal/api/client/products/ \
-  --output ./apiv1/mocks --filename client_service.go --structname MockClientService --log-level debug
+  --output ./apiv2/mocks --filename client_service.go --structname MockClientService --log-level debug
   printf "%s\n" 1 i "${BUILD_FLAG}" . w | ed -s "${MOCK_FILE}" &>/dev/null
 fi
