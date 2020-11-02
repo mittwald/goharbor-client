6ced3ef347bf6ae50c4038a2dd9a305a38ea2ef7
Dominik Korittki <23359034+dkorittki@users.noreply.github.com>
2020-11-02 10:53:40 +0100

    Update helm chart url
    kubernetes-charts.storage.googleapis.com is deprecated,
    charts.helm.sh/stable should be used instead

--- scripts/setup-harbor.sh
+++ scripts/setup-harbor.sh
@@ -93,7 +93,7 @@ if [[ "$?" -ne "0" ]]; then
 fi
 
 echo "Installing seperate docker registry for integration tests..."
-helm repo add stable https://kubernetes-charts.storage.googleapis.com && helm repo update
+helm repo add stable https://charts.helm.sh/stable && helm repo update
 helm install registry stable/docker-registry \
     --set service.port=5000,image.tag=${REGISTRY_IMAGE_TAG}
 if [[ "$?" -ne "0" ]]; then
