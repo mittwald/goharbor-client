adbbe7b12806f94d058cc5d5300d78d1d8250802
Eike David Lenz <e.lenz@mittwald.de>
2021-10-26 09:50:49 +0200

    deprecate legacyapi from systeminfo client

--- apiv2/systeminfo/systeminfo_integration_test.go
+++ apiv2/systeminfo/systeminfo_integration_test.go
@@ -1,4 +1,4 @@
-////go:build integration
+//go:build integration
 
 package systeminfo
 
@@ -15,16 +15,14 @@ import (
 	integrationtest "github.com/mittwald/goharbor-client/v5/apiv2/pkg/testing"
 
 	v2client "github.com/mittwald/goharbor-client/v5/apiv2/internal/api/client"
-	"github.com/mittwald/goharbor-client/v5/apiv2/internal/legacyapi/client"
 )
 
 var (
-	u, _                = url.Parse(integrationtest.Host)
-	legacySwaggerClient = client.New(runtimeclient.New(u.Host, u.Path, []string{u.Scheme}), strfmt.Default)
-	v2SwaggerClient     = v2client.New(runtimeclient.New(u.Host, u.Path, []string{u.Scheme}), strfmt.Default)
-	authInfo            = runtimeclient.BasicAuth(integrationtest.User, integrationtest.Password)
-	opts                = config.Options{}
-	defaultOpts         = opts.Defaults()
+	u, _            = url.Parse(integrationtest.Host)
+	v2SwaggerClient = v2client.New(runtimeclient.New(u.Host, u.Path, []string{u.Scheme}), strfmt.Default)
+	authInfo        = runtimeclient.BasicAuth(integrationtest.User, integrationtest.Password)
+	opts            = config.Options{}
+	defaultOpts     = opts.Defaults()
 )
 
 func TestAPIGetSystemInfo(t *testing.T) {
@@ -34,7 +32,7 @@ func TestAPIGetSystemInfo(t *testing.T) {
 	resp, err := c.GetSystemInfo(ctx)
 	require.NoError(t, err)
 
-	require.Equal(t,false,  *resp.WithChartmuseum )
+	require.Equal(t, false, *resp.WithChartmuseum)
 	require.Equal(t, false, *resp.WithNotary)
 	require.Equal(t, *resp.RegistryURL, "localhost")
 }
