1ed5440e9d09eb0ca6b6628935167978f92a11af
Joan Miquel Luque Oliver <joan.luque@dynatrace.com>
2023-08-07 14:45:00 +0200

    GH-169: Fix integration tests

--- apiv2/pkg/clients/configure/configure_integration_test.go
+++ apiv2/pkg/clients/configure/configure_integration_test.go
@@ -6,6 +6,9 @@ import (
 	"context"
 	"testing"
 
+	modelv2 "github.com/mittwald/goharbor-client/v5/apiv2/model"
+	clienttesting "github.com/mittwald/goharbor-client/v5/apiv2/pkg/testing"
+
 	"github.com/stretchr/testify/require"
 )
 
